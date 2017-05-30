a5ffed4beb1c62d3bcfc922519f3cc066f106175
SoriS <pashanikoluyk@mail.ru>
2017-05-30 19:34:56 +0300

    object cell

--- controller.js
+++ controller.js
@@ -1,3 +1,4 @@
+
 app.controller('SnapCont', ['$scope', function ($scope) {
     $scope.formData = {
         height: 16,
@@ -19,31 +20,49 @@ app.controller('SnapCont', ['$scope', function ($scope) {
             $scope.Field.push([])
 
             for (var j = 0; j < $scope.fieldSize.width; j++) {
-                $scope.Field[i].push({i:i,j:j})
+                $scope.Field[i].push(new Cell(i, j))
+
             }
         }
         console.log($scope.Field);
         $scope.game_start = true;
+        console.log($scope.fieldSize.height, $scope.fieldSize.width)
+
     }
+
+
     $scope.show = function (item) {
-        alert(' i: '+ item.i+' j: '+item.j)
+
+        var Ran_i = Math.floor(Math.random() * $scope.fieldSize.height);
+        var Ran_j = Math.floor(Math.random() * $scope.fieldSize.width);
+        var NumMimeCell = Math.floor(($scope.fieldSize.height * $scope.fieldSize.width) / 7)
+
+        item.click = true;
+        
+        for (var n = 0; n < item.posi+1; n++) {
+            var find_cell = $scope.Field[n].find( (e)=> {
+                return e.posi == item.posi && e.posj == item.posj;
+            });
+        }
+
+
+        console.log(find_cell)
+
+        for (p = 0; p < NumMimeCell; p++) {
+
+        }
+
     }
-    // cell = {
-    //     pos: [$scope.formData.width, $scope.formData.height],
-    //     mine: false,   //міна
-    //     click: false,  //чи активована нажатієм
-    //     check: false,  //чи встановлений флажок
-    //     mineNum: 0     //кількість мін біля ячейки
-    // }
-    // function Cell(cPosH, cPosW, cMine, cClick, cCheck, cMineNum) {
-    //     this.pos
-    //     this.mine
-    //     this.click
-    //     this.check
-    //     this.mineNum
-    // }
 
-
-
+    function Cell(cPosI, cPosJ, cMine, cClick, cCheck, cMineNum) {
+        this.posi = cPosI;
+        this.posj = cPosJ;
+        this.mine = !!cMine;
+        this.click = !!cClick;
+        this.check = !!cCheck;
+        this.mineNum = cMineNum || 0;
+        // this.ShowConsole = function (){
+        //    console.log('i = ' + this.posi + ' j= ' + this.posj)}
+    }
 
 }])
