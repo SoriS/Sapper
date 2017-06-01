7a4363fbabb64fc54a79b2a467e786839943397d
SoriS <pashanikoluyk@mail.ru>
2017-06-02 02:35:56 +0300

    nazar code

--- controller.js
+++ controller.js
@@ -32,17 +32,35 @@ app.controller('SnapCont', ['$scope', function ($scope) {
 
     }
 
+    $scope.clickByCell = function (item) {
+        item.click = true;
+        if (Generate_Mine.count === 0) {
+            $scope.show(item); // generate field
+            Search_mine_number(item); // search mines
+            return
+        }
+
+        if (item.mine === true) {
+            console.log("End_Game")
+            //need to show all mins and end the game
+        }
+        else {
+
+            if (item.mineNum === 0) {
+                Search_mine_number(item);
+
+            }
+            console.log(Find_cell_item(item))
+            // recursion call
+            //and show number of mins
+        }
+    }
 
     $scope.show = function (item) {
-
-        item.click = true;
         console.log(Find_cell_item(item))
-
-        if (Generate_Mine.count === 0)
-        { Generate_Mine(item) }
-
-        Search_mine_number(item)
-
+        if (Generate_Mine.count === 0) {
+            Generate_Mine(item)
+        }
     }
 
     function Search_mine_number(item) {
@@ -53,26 +71,45 @@ app.controller('SnapCont', ['$scope', function ($scope) {
         var Indexes_i = getIndexes($scope.Field, ic)
         var Indexes_j = getIndexes($scope.Field[ic], jc)
 
-        var thisArr = {
-            topLeft: $scope.Field[Indexes_i.prev][Indexes_j.prev],
-            top: $scope.Field[Indexes_i.prev][Indexes_j.curr],
-            topRight: $scope.Field[Indexes_i.prev][Indexes_j.next],
-            centerLeft: $scope.Field[Indexes_i.curr][Indexes_j.prev],
-            center: $scope.Field[Indexes_i.curr][Indexes_j.curr],
-            centerRight: $scope.Field[Indexes_i.curr][Indexes_j.next],
-            bottomLeft: $scope.Field[Indexes_i.next][Indexes_j.prev],
-            bottom: $scope.Field[Indexes_i.next][Indexes_j.curr],
-            bottomRight: $scope.Field[Indexes_i.next][Indexes_j.next],
-        }
+        var results = [];
+        console.log("Element:")
+
+
+        var thisArr = [
+            [Indexes_i.prev, Indexes_j.prev],
+            [Indexes_i.prev, Indexes_j.curr],
+            [Indexes_i.prev, Indexes_j.next],
+            [Indexes_i.curr, Indexes_j.prev],
+            [Indexes_i.curr, Indexes_j.curr],
+            [Indexes_i.curr, Indexes_j.next],
+            [Indexes_i.next, Indexes_j.prev],
+            [Indexes_i.next, Indexes_j.curr],
+            [Indexes_i.next, Indexes_j.next]
+
+        ]
+
+        console.log(thisArr.length)
+        console.log(thisArr[4])
+        // var thisArr = [
+        //     { topLeft: $scope.Field[Indexes_i.prev][Indexes_j.prev] },
+        //     { top: $scope.Field[Indexes_i.prev][Indexes_j.curr] },
+        //     { topRight: $scope.Field[Indexes_i.prev][Indexes_j.next] },
+        //     { centerLeft: $scope.Field[Indexes_i.curr][Indexes_j.prev] },
+        //     { center: $scope.Field[Indexes_i.curr][Indexes_j.curr] },
+        //     { centerRight: $scope.Field[Indexes_i.curr][Indexes_j.next] },
+        //     { bottomLeft: $scope.Field[Indexes_i.next][Indexes_j.prev] },
+        //     { bottom: $scope.Field[Indexes_i.next][Indexes_j.curr] },
+        //     { bottomRight: $scope.Field[Indexes_i.next][Indexes_j.next] }
+        // ]
 
 
         function getIndexes(Field, ic) {
             let prev = ic - 1;
             let next = ic + 1;
             if (ic == 0) {
-                prev = $scope.Field.length - 1;
+                prev = $scope.Field.length;
             } else if (ic == $scope.Field.length - 1) {
-                next = 0;
+                next = $scope.Field.length;
             }
 
             return {
@@ -81,13 +118,29 @@ app.controller('SnapCont', ['$scope', function ($scope) {
                 next: next
             };
         }
-        console.log(thisArr);
-        for(var x in thisArr){
-            if(thisArr[x].mine ===true)
-            {item.mineNum++}
+
+
+
+       thisArr = thisArr.filter(c=>c[0]!=$scope.fieldSize.width && c[1] != $scope.fieldSize.height);// thisArr.filter(filterByPos, thisArr)
+        debugger
+
+        for (var i = 0; i < thisArr.length; i++) {
             
-        console.log(thisArr[x],item.mineNum);
+            // @ts-ignore
+            var tmp = $scope.Field[thisArr[i][0]]
+            item = tmp.find((e) => {
+                return e.posj == thisArr[i][1];
+            });
+            console.log(item)
+            if(!item){
+                debugger
+            }
+            if (item.mine === true) {
+                $scope.Field[Indexes_i.curr][Indexes_j.curr].mineNum++;
+            }
         }
+
+
     }
 
     function Generate_Mine(item) {
@@ -107,15 +160,11 @@ app.controller('SnapCont', ['$scope', function ($scope) {
                 p--;
             }
             else {
-                find_cell.click = true;
+                find_cell.click = false;
                 find_cell.mine = true;
                 item = find_cell;
                 console.log(item, p)
             }
-
-
-
-
         }
         Generate_Mine.count++;
     }
@@ -123,14 +172,24 @@ app.controller('SnapCont', ['$scope', function ($scope) {
 
     function Find_cell_item(item) {
 
-        for (var n = 0; n < item.posi + 1; n++) {
-            var find_cell_local = $scope.Field[n].find((e) => {
-                return e.posi == item.posi && e.posj == item.posj;
-            });
-        }
+
+        var find_cell_local = $scope.Field[item.posi].find((e) => {
+            return e.posi == item.posi && e.posj == item.posj;
+        });
+
         return find_cell_local;
     }
+    function filterByPos(obj, thisArr) {
+        for (var i = 0; i < thisArr.length; i++) {
+            if (obj.thisArr[i][0] >= $scope.Field.length && obj.thisArr[i][1] >= $scope.Field.length) {
+                return false;
+            }
+            else {
+                return true;
 
+            }
+        }
+    }
 
     function Cell(cPosI, cPosJ, cMine, cClick, cCheck, cMineNum) {
         this.posi = cPosI;
@@ -139,8 +198,6 @@ app.controller('SnapCont', ['$scope', function ($scope) {
         this.click = !!cClick;
         this.check = !!cCheck;
         this.mineNum = cMineNum || 0;
-        // this.ShowConsole = function (){
-        //    console.log('i = ' + this.posi + ' j= ' + this.posj)}
     }
 
 }])
