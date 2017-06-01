e278937e6c796a58da4fe702c58dc573c0eb2a86
SoriS <pashanikoluyk@mail.ru>
2017-06-01 16:41:36 +0300

    refacktor code

--- controller.js
+++ controller.js
@@ -9,8 +9,10 @@ app.controller('SnapCont', ['$scope', function ($scope) {
         height: 0,
         width: 0,
     }
+
     $scope.Field = [];
     $scope.submit = function () {
+
 
         $scope.fieldSize.height = $scope.formData.height;
         $scope.fieldSize.width = $scope.formData.width;
@@ -34,57 +36,37 @@ app.controller('SnapCont', ['$scope', function ($scope) {
     $scope.show = function (item) {
 
         item.click = true;
+        console.log(Find_cell_item(item))
 
-        for (var n = 0; n < item.posi + 1; n++) {
-            var find_cell = $scope.Field[n].find((e) => {
-                return e.posi == item.posi && e.posj == item.posj;
-            });
-        }
+        if (Generate_Mine.count === 0)
+        { Generate_Mine(item) }
 
-        console.log(find_cell)
-        var NumMimeCell = Math.floor(($scope.fieldSize.height * $scope.fieldSize.width) / 7)
+        Search_mine_num(item)
 
-        for (var p = 0; p < NumMimeCell; p++) {
-            var Ran_i = Math.floor(Math.random() * $scope.fieldSize.height);
-            var Ran_j = Math.floor(Math.random() * $scope.fieldSize.width);
-
-            var find_cell = $scope.Field[Ran_i].find((e) => {
-                return e.posi == Ran_i && e.posj == Ran_j;
-            });
-            if (find_cell.mine === true)
-            { p-- }
-
-            find_cell.mine = true;
-            item = find_cell;
-
-
-            // console.log(item)
-
-
-        }
     }
-    $scope.click_cell = function (item) {
-        debugger
+
+    function Search_mine_num(item) {
+
         var ic = item.posi;
         var jc = item.posj
 
-        var Indexes_i = getIndexes( $scope.Field, ic)
-        var Indexes_j = getIndexes( $scope.Field[ic], jc)
+        var Indexes_i = getIndexes($scope.Field, ic)
+        var Indexes_j = getIndexes($scope.Field[ic], jc)
 
         var thisArr = {
-            topLeft:  $scope.Field[Indexes_i.prev][Indexes_j.prev],
-            top:  $scope.Field[Indexes_i.prev][Indexes_j.curr],
-            topRight:  $scope.Field[Indexes_i.prev][Indexes_j.next],
-            centerLeft:  $scope.Field[Indexes_i.curr][Indexes_j.prev],
-            center:  $scope.Field[Indexes_i.curr][Indexes_j.curr],
-            centerRight:  $scope.Field[Indexes_i.curr][Indexes_j.next],
-            bottomLeft:  $scope.Field[Indexes_i.next][Indexes_j.prev],
-            bottom:  $scope.Field[Indexes_i.next][Indexes_j.curr],
-            bottomRight:  $scope.Field[Indexes_i.next][Indexes_j.next],
+            topLeft: $scope.Field[Indexes_i.prev][Indexes_j.prev],
+            top: $scope.Field[Indexes_i.prev][Indexes_j.curr],
+            topRight: $scope.Field[Indexes_i.prev][Indexes_j.next],
+            centerLeft: $scope.Field[Indexes_i.curr][Indexes_j.prev],
+            center: $scope.Field[Indexes_i.curr][Indexes_j.curr],
+            centerRight: $scope.Field[Indexes_i.curr][Indexes_j.next],
+            bottomLeft: $scope.Field[Indexes_i.next][Indexes_j.prev],
+            bottom: $scope.Field[Indexes_i.next][Indexes_j.curr],
+            bottomRight: $scope.Field[Indexes_i.next][Indexes_j.next],
         }
 
 
-        function getIndexes( Field, ic) {
+        function getIndexes(Field, ic) {
             let prev = ic - 1;
             let next = ic + 1;
             if (ic == 0) {
@@ -102,6 +84,46 @@ app.controller('SnapCont', ['$scope', function ($scope) {
         console.log(thisArr);
     }
 
+    function Generate_Mine(item) {
+
+        var NumMimeCell = Math.floor(($scope.fieldSize.height * $scope.fieldSize.width) / 7)
+
+        for (var p = 0; p < NumMimeCell; p++) {
+            var Ran_i = Math.floor(Math.random() * $scope.fieldSize.height);
+            var Ran_j = Math.floor(Math.random() * $scope.fieldSize.width);
+
+            var find_cell = $scope.Field[Ran_i].find((e) => {
+                return e.posi == Ran_i && e.posj == Ran_j;
+            });
+
+
+            if (find_cell.click === true) {
+                p--;
+            }
+            else {
+                find_cell.click = true;
+                find_cell.mine = true;
+                item = find_cell;
+                console.log(item, p)
+            }
+
+
+
+
+        }
+        Generate_Mine.count++;
+    }
+    Generate_Mine.count = 0;
+
+    function Find_cell_item(item) {
+
+        for (var n = 0; n < item.posi + 1; n++) {
+            var find_cell_local = $scope.Field[n].find((e) => {
+                return e.posi == item.posi && e.posj == item.posj;
+            });
+        }
+        return find_cell_local;
+    }
 
 
     function Cell(cPosI, cPosJ, cMine, cClick, cCheck, cMineNum) {
